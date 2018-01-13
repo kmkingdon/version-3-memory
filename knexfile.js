73664e2c041f67adbe99543bb900a114545f3f43
kmkingdon <32685092+kmkingdon@users.noreply.github.com>
2018-01-13 11:43:19 -0700

    version-3 knex database

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: "postgres://localhost/gamedb"
+  },
+  production: {
+    client: "pg",
+    connection: `${process.env.DATABASE_URL}?ssl=true`
+  }
+}
